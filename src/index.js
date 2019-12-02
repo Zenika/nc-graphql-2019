4ebdd2d1b52e4b3c2313dddd3fb04d437369f35d
Alexis Brones <alexis.brones@zenika.com>
2019-12-02 20:09:18 +0100

    Refactor externalize schema

--- src/index.js
+++ src/index.js
@@ -7,19 +7,6 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
@@ -34,7 +21,7 @@ const resolvers = {
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
