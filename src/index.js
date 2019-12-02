83472bded751188b1a648bfe02f0f7ff9836f9e4
Alexis Brones <alexis.brones@zenika.com>
2019-12-02 20:04:03 +0100

    One more simple query

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 
-// 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
