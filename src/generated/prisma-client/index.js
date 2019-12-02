ef6fd418ce49bb7ebafc428dcd8be3825b0bde58
Alexis Brones <alexis.brones@zenika.com>
2019-12-02 20:58:42 +0100

    Add Prisma on demo mode

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/alexis-brones-0fc289/nc-graphql-2019/dev`
+});
+exports.prisma = new exports.Prisma();
