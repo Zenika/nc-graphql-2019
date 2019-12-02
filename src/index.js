5c9bd136543e4662737412d3290dcf734bd6f115
Alexis Brones <alexis.brones@zenika.com>
2019-12-02 18:57:57 +0100

    Init GraphQL Node & first query

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
-const Query = require('./resolvers/query')
 
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+// 2
 const resolvers = {
-  Query
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
 }
 
+// 3
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs,
   resolvers,
-  context: { prisma }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
